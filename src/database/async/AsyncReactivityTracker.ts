6087a13a2aee948540ea81ec1ab50ac65635e9da
Matthew Linkous <matthew.linkous@gmail.com>
2023-12-11 19:43:30 -0600

    Make reactivity emits much faster

--- src/database/async/AsyncReactivityTracker.ts
+++ src/database/async/AsyncReactivityTracker.ts
@@ -1,25 +1,22 @@
 import { maybePromiseAll } from "../../helpers/maybeWaitForPromises"
-import { randomId } from "../../helpers/randomId"
-import {
-	Bounds,
-	getPrefixContainingBounds,
-	isTupleWithinBounds,
-} from "../../helpers/sortedTupleArray"
-import { InMemoryTupleStorage } from "../../storage/InMemoryTupleStorage"
-import { MIN, ScanStorageArgs, Tuple, WriteOps } from "../../storage/types"
-import { TupleStorageApi } from "../sync/types"
+import { Bounds } from "../../helpers/sortedTupleArray"
+import { ScanStorageArgs, WriteOps } from "../../storage/types"
 import { TxId } from "../types"
 import { AsyncCallback } from "./asyncTypes"
+import * as SortedTupleValue from "../../helpers/sortedTupleValuePairs"
+import * as SortedTuple from "../../helpers/sortedTupleArray"
+
+type Listeners = Map<AsyncCallback, Bounds>
 
 export class AsyncReactivityTracker {
-	private listenersDb = new InMemoryTupleStorage()
+	private listeners: Listeners = new Map()
 
 	subscribe(args: ScanStorageArgs, callback: AsyncCallback) {
-		return subscribe(this.listenersDb, args, callback)
+		return subscribe(this.listeners, args, callback)
 	}
 
 	computeReactivityEmits(writes: WriteOps) {
-		return getReactivityEmits(this.listenersDb, writes)
+		return getReactivityEmits(this.listeners, writes)
 	}
 
 	async emit(emits: ReactivityEmits, txId: TxId) {
@@ -39,78 +36,16 @@ export class AsyncReactivityTracker {
 	}
 }
 
-type AsyncListener = { callback: AsyncCallback; bounds: Bounds }
-
-function iterateTuplePrefixes(tuple: Tuple) {
-	const prefixes: Tuple[] = [tuple]
-	for (let i = 0; i < tuple.length; i++) {
-		const prefix = tuple.slice(0, i)
-		prefixes.push(prefix)
-	}
-	return prefixes
-}
-
-/** Query the listenersDb based on tuple prefixes. */
-function getListenersForTuplePrefix(
-	listenersDb: TupleStorageApi,
-	tuple: Tuple
-) {
-	const listeners: AsyncListener[] = []
-
-	// Look for listeners at each prefix of the tuple.
-	for (const prefix of iterateTuplePrefixes(tuple)) {
-		const results = listenersDb.scan({
-			gte: [prefix],
-			lte: [[...prefix, MIN]],
-		})
-
-		for (const { value } of results) {
-			listeners.push(value)
-		}
-	}
-
-	return listeners
-}
-
-/** Query the listenersDb based on tuple prefixes, and additionally check for query bounds. */
-function getListenerCallbacksForTuple(
-	listenersDb: TupleStorageApi,
-	tuple: Tuple
-) {
-	const callbacks: AsyncCallback[] = []
-
-	// Check that the tuple is within the absolute bounds of the query.
-	for (const listener of getListenersForTuplePrefix(listenersDb, tuple)) {
-		const { callback, bounds } = listener
-		if (isTupleWithinBounds(tuple, bounds)) {
-			callbacks.push(callback)
-		} else {
-			// TODO: track how in-efficient listeners are here.
-			// NOTE: the bounds may only partially span the prefix.
-		}
-	}
-
-	return callbacks
-}
-
 type ReactivityEmits = Map<AsyncCallback, Required<WriteOps>>
 
-function getReactivityEmits(listenersDb: TupleStorageApi, writes: WriteOps) {
+function getReactivityEmits(listenersDb: Listeners, writes: WriteOps) {
 	const emits: ReactivityEmits = new Map()
 
-	for (const { key, value } of writes.set || []) {
-		const callbacks = getListenerCallbacksForTuple(listenersDb, key)
-		for (const callback of callbacks) {
-			if (!emits.has(callback)) emits.set(callback, { set: [], remove: [] })
-			emits.get(callback)!.set.push({ key, value })
-		}
-	}
-
-	for (const tuple of writes.remove || []) {
-		const callbacks = getListenerCallbacksForTuple(listenersDb, tuple)
-		for (const callback of callbacks) {
-			if (!emits.has(callback)) emits.set(callback, { set: [], remove: [] })
-			emits.get(callback)!.remove.push(tuple)
+	for (const [callback, bounds] of listenersDb) {
+		const matchingWrites = SortedTupleValue.scan(writes.set || [], bounds)
+		const matchingRemoves = SortedTuple.scan(writes.remove || [], bounds)
+		if (matchingWrites.length > 0 || matchingRemoves.length > 0) {
+			emits.set(callback, { set: matchingWrites, remove: matchingRemoves })
 		}
 	}
 
@@ -118,23 +53,13 @@ function getReactivityEmits(listenersDb: TupleStorageApi, writes: WriteOps) {
 }
 
 function subscribe(
-	listenersDb: TupleStorageApi,
+	listenersDb: Listeners,
 	args: ScanStorageArgs,
 	callback: AsyncCallback
 ) {
-	// this.log("db/subscribe", args)
+	listenersDb.set(callback, args)
 
-	const prefix = getPrefixContainingBounds(args)
-
-	const id = randomId()
-	const value: AsyncListener = { callback, bounds: args }
-
-	listenersDb.commit({ set: [{ key: [prefix, id], value }] })
-
-	const unsubscribe = () => {
-		// this.log("db/unsubscribe", args)
-		listenersDb.commit({ remove: [[prefix, id]] })
+	return () => {
+		listenersDb.delete(callback)
 	}
-
-	return unsubscribe
 }
