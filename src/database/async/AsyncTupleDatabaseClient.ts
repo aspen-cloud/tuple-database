ca42b2c8c0f4a5916de779d1217e68b7ac38c78d
Will Ernst <wernst@users.noreply.github.com>
2024-05-28 19:11:35 -0700

    revert change to set

--- src/database/async/AsyncTupleDatabaseClient.ts
+++ src/database/async/AsyncTupleDatabaseClient.ts
@@ -9,7 +9,7 @@ import {
 	removePrefixFromTupleValuePairs,
 	removePrefixFromWriteOps,
 } from "../../helpers/subspaceHelpers"
-import { decodeTuple, encodeTuple } from "../../main"
+import { compareTuple } from "../../main"
 import { KeyValuePair, Tuple, WriteOps } from "../../storage/types"
 import { TupleDatabaseApi } from "../sync/types"
 import {
@@ -117,47 +117,39 @@ export class AsyncTupleDatabaseClient<S extends KeyValuePair = KeyValuePair>
 export class AsyncTupleRootTransaction<S extends KeyValuePair>
 	implements AsyncTupleRootTransactionApi<S>
 {
-	committed = false
-	canceled = false
-	private _writes: { set: S[]; remove: Set<string> }
-
-	// Track whether writes are dirty and need to be sorted prior to reading
-	private setsDirty = false
-
 	constructor(
 		private db: AsyncTupleDatabaseApi | TupleDatabaseApi,
 		public subspacePrefix: Tuple,
 		public id: TxId,
 		writes?: WriteOps<S>
 	) {
-		this._writes = {
-			set: writes?.set ?? [],
-			remove:
-				writes?.remove === undefined
-					? new Set()
-					: new Set(writes.remove.map((k) => encodeTuple(k))),
-		}
+		this.writes = { set: [], remove: [], ...writes }
 	}
 
-	get writes() {
-		return {
-			set: this._writes.set.filter(({ key }) => {
-				const encodedKey = encodeTuple(key)
-				const isRemoved = this._writes.remove.has(encodedKey)
-				return !isRemoved
-			}),
-			remove: Array.from(this._writes.remove).map(decodeTuple),
-		}
-	}
+	committed = false
+	canceled = false
+	writes: Required<WriteOps<S>>
+
+	// Track whether writes are dirty and need to be sorted prior to reading
+	private setsDirty = false
+	private removesDirty = false
 
 	private cleanWrites() {
 		this.cleanSets()
+		this.cleanRemoves()
 	}
 
 	private cleanSets() {
 		if (this.setsDirty) {
-			this._writes.set = this._writes.set.sort(tv.compareTupleValuePair)
+			this.writes.set = this.writes.set.sort(tv.compareTupleValuePair)
 			this.setsDirty = false
+		}
+	}
+
+	private cleanRemoves() {
+		if (this.removesDirty) {
+			this.writes.remove = this.writes.remove.sort(compareTuple)
+			this.removesDirty = false
 		}
 	}
 
@@ -178,39 +170,26 @@ export class AsyncTupleRootTransaction<S extends KeyValuePair>
 		)
 
 		// We don't want to include the limit in this scan.
-		const txTuples = tv.scan(this._writes.set, scanArgs)
+		const sets = tv.scan(this.writes.set, scanArgs)
+		const removes = t.scan(this.writes.remove, scanArgs)
 
 		// If we've removed items from this range, then lets make sure to fetch enough
 		// from storage for the final result limit.
-		const scanLimit = resultLimit
-			? resultLimit + this._writes.remove.size
-			: undefined
-		const dbTuples = await this.db.scan(
-			{ ...scanArgs, limit: scanLimit },
-			this.id
-		)
-		let result = dbTuples
+		const scanLimit = resultLimit ? resultLimit + removes.length : undefined
 
-		for (const { key: tuple, value } of txTuples) {
+		const pairs = await this.db.scan({ ...scanArgs, limit: scanLimit }, this.id)
+		const result = removePrefixFromTupleValuePairs(this.subspacePrefix, pairs)
+
+		for (const { key: fullTuple, value } of sets) {
+			const tuple = removePrefixFromTuple(this.subspacePrefix, fullTuple)
 			// Make sure we insert in reverse if the scan is in reverse.
 			tv.set(result, tuple, value, scanArgs.reverse)
 		}
-
-		if (this._writes.remove.size > 0) {
-			result = result.filter(
-				({ key }) => !this._writes.remove.has(encodeTuple(key))
-			)
+		for (const fullTuple of removes) {
+			const tuple = removePrefixFromTuple(this.subspacePrefix, fullTuple)
+			tv.remove(result, tuple, scanArgs.reverse)
 		}
 
-		if (this.subspacePrefix && this.subspacePrefix.length > 0) {
-			result = result.map(
-				(tuple) =>
-					({
-						key: removePrefixFromTuple(this.subspacePrefix, tuple.key),
-						value: tuple.value,
-					} as S)
-			)
-		}
 		// Make sure to truncate the results if we added items to the result set.
 		if (resultLimit) {
 			if (result.length > resultLimit) {
@@ -228,11 +207,11 @@ export class AsyncTupleRootTransaction<S extends KeyValuePair>
 		this.cleanWrites()
 		const fullTuple = prependPrefixToTuple(this.subspacePrefix, tuple)
 
-		if (tv.exists(this._writes.set, fullTuple)) {
+		if (tv.exists(this.writes.set, fullTuple)) {
 			// TODO: binary searching twice unnecessarily...
-			return tv.get(this._writes.set, fullTuple)
+			return tv.get(this.writes.set, fullTuple)
 		}
-		if (this._writes.remove.has(encodeTuple(fullTuple))) {
+		if (t.exists(this.writes.remove, fullTuple)) {
 			return
 		}
 		const items = await this.db.scan(
@@ -250,13 +229,12 @@ export class AsyncTupleRootTransaction<S extends KeyValuePair>
 		this.cleanWrites()
 		const fullTuple = prependPrefixToTuple(this.subspacePrefix, tuple)
 
-		if (this._writes.remove.has(encodeTuple(fullTuple))) {
-			return false
-		}
-		if (tv.exists(this._writes.set, fullTuple)) {
+		if (tv.exists(this.writes.set, fullTuple)) {
 			return true
 		}
-
+		if (t.exists(this.writes.remove, fullTuple)) {
+			return false
+		}
 		const items = await this.db.scan(
 			{ gte: fullTuple, lte: fullTuple },
 			this.id
@@ -271,24 +249,28 @@ export class AsyncTupleRootTransaction<S extends KeyValuePair>
 		value: T["value"]
 	): AsyncTupleRootTransactionApi<S> {
 		this.checkActive()
+		this.cleanRemoves()
 		const fullTuple = prependPrefixToTuple(this.subspacePrefix, tuple)
-		this._writes.remove.delete(encodeTuple(fullTuple))
-		this._writes.set.push({ key: fullTuple, value: value } as S)
+		t.remove(this.writes.remove, fullTuple)
+		this.writes.set.push({ key: fullTuple, value: value } as S)
 		this.setsDirty = true
 		return this
 	}
 
 	remove(tuple: S["key"]): AsyncTupleRootTransactionApi<S> {
 		this.checkActive()
+		this.cleanSets()
 		const fullTuple = prependPrefixToTuple(this.subspacePrefix, tuple)
-		this._writes.remove.add(encodeTuple(fullTuple))
+		tv.remove(this.writes.set, fullTuple)
+		this.writes.remove.push(fullTuple)
+		this.removesDirty = true
 		return this
 	}
 
 	write(writes: WriteOps<S>): AsyncTupleRootTransactionApi<S> {
 		this.checkActive()
 
-		// If you're calling this function, then the order of these operations
+		// If you're calling this function, then the order of these opertions
 		// shouldn't matter.
 		const { set, remove } = writes
 		for (const tuple of remove || []) {
@@ -303,7 +285,6 @@ export class AsyncTupleRootTransaction<S extends KeyValuePair>
 	async commit() {
 		this.checkActive()
 		this.committed = true
-		// TODO should just encode tuples to strings before passing to storage
 		return this.db.commit(this.writes, this.id)
 	}
 
